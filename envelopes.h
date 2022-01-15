023adb69be6e87468dd311bc7cc631d33438d10a
Geraint <luffgd@gmail.com>
2022-01-15 02:57:33 +0000

    Add peak-hold filter

--- envelopes.h
+++ envelopes.h
@@ -183,15 +183,14 @@ namespace envelopes {
 	public:
 		BoxFilter(int maxSize) : boxSum(maxSize) {
 			resize(maxSize);
-			set(maxSize);
 		}
-		/// Sets the maximum size (and reset contents)
+		/// Sets the maximum size (and current size, and resets)
 		void resize(int maxSize) {
 			_maxSize = maxSize;
 			boxSum.resize(maxSize);
-			if (maxSize < _size) set(maxSize);
+			set(maxSize);
 		}
-		/// Sets the current size (expanding size if needed)
+		/// Sets the current size (expanding/allocating only if needed)
 		void set(int size) {
 			_size = size;
 			multiplier = Sample(1)/size;
@@ -244,7 +243,6 @@ namespace envelopes {
 	public:
 		BoxStackFilter(int maxSize, int layers=4) {
 			resize(maxSize, layers);
-			set(maxSize);
 		}
 		
 		/** Approximate bandwidth for a given number of layers
@@ -260,13 +258,14 @@ namespace envelopes {
 			return 5 - layers*18;
 		}
 		
-		/// Sets the maximum impulse response length and layer count
+		/// Sets the maximum (and current) impulse response length and layer count, and resets the filter.
 		void resize(int maxSize, int layerCount) {
 			if (int(layers.size()) != layerCount) setupLayers(layerCount);
 			for (auto &layer : layers) {
 				layer.filter.resize(int(maxSize*layer.ratio + 2));
 			}
-			if (maxSize < _size) set(maxSize);
+			set(maxSize);
+			reset();
 		}
 		
 		/// Sets the impulse response length (does not reset if `size` ≤ `maxSize`)
@@ -310,6 +309,93 @@ namespace envelopes {
 				v = layer.filter(v);
 			}
 			return v;
+		}
+	};
+	
+	/** Peak-hold filter.
+		\diagram{peak-hold.svg}
+		
+		The size is variable, and can be changed instantly with `.set()`, or by using `.push()`/`.pop()` in an unbalanced way.
+
+		To guarantee no allocations while running, it uses a fixed-size array (not a `std::deque`) which determines the maximum length.
+	*/
+	template<typename Sample=double>
+	class PeakHold {
+		int frontIndex = 0, backIndex = 0;
+		int indexMask;
+		struct Segment {
+			Sample value;
+			int length;
+		};
+		std::vector<Segment> segments;
+	public:
+		PeakHold(int maxLength) {
+			resize(maxLength);
+		}
+		/// Sets the maximum guaranteed size.
+		void resize(int maxLength) {
+			int length = 1;
+			while (length < maxLength) length *= 2;
+			indexMask = length - 1;
+			
+			frontIndex = backIndex = 0;
+			// Sets the size
+			segments.assign(length, Segment{0, maxLength});
+		}
+		/// Calculates the current size
+		int size() const {
+			int result = 0;
+			for (int i = frontIndex; i <= backIndex; ++i) {
+				result += segments[i&indexMask].length;
+			}
+			return result;
+		}
+		/// Resets the filter, preserving the size
+		void reset(Sample fill=Sample()) {
+			int s = size();
+			frontIndex = backIndex = 0;
+			segments[0] = {fill, s};
+		}
+		/// Sets a new size, extending the oldest value if needed
+		void set(int newSize) {
+			int oldSize = size();
+			if (newSize > oldSize) {
+				auto &front = segments[frontIndex&indexMask];
+				front.length += newSize - oldSize;
+			} else {
+				for (int i = 0; i < oldSize - newSize; ++i) {
+					pop();
+				}
+			}
+		}
+
+		/// Drops the oldest value from the peak tracker.
+		void pop() {
+			auto &front = segments[frontIndex&indexMask];
+			if (--front.length == 0) {
+				++frontIndex;
+			}
+		}
+		/// Adds a value to the peak tracker
+		void push(Sample v) {
+			int length = 1;
+			while (backIndex >= frontIndex && segments[backIndex&indexMask].value <= v) {
+				// Consume the segment
+				length += segments[backIndex&indexMask].length;
+				--backIndex;
+			}
+			++backIndex;
+			segments[backIndex&indexMask] = {v, length};
+		}
+		/// Returns the current value
+		Sample read() const {
+			return segments[frontIndex&indexMask].value;
+		}
+		/// Adds a new value and drops an old one.
+		Sample operator()(Sample v) {
+			pop();
+			push(v);
+			return read();
 		}
 	};
 
